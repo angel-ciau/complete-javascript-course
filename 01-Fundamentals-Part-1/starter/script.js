21cc7674e490cb6d15bc6d80b1cb6ebc6e50eb75
angelarroyo <aac@wayakna.com>
2023-06-07 12:51:11 -0600

    Section 1 day 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,82 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firsName = "Bob";
+// console.log(firsName);
+// console.log(firsName);
+// console.log(firsName);
+
+// /* Assignments */
+
+// let country = "Mexico";
+// let continent = "America";
+// let population = "10";
+
+// console.log(country, continent, population)
+
+// let javascriptIsFun = true;
+// console.log(true);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javascriptIsFun = "YES!";
+
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1993;
+// // birthYear = 2020;
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas/10, 2**3)
+
+// const firstName = "Angel";
+// const secondName = "Ciau";
+
+// console.log(firstName + " " + secondName);
+
+// let x = 10 + 5;
+// x += 5;
+// console.log(x);
+
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = 1992;
+// const year = 2037
+
+// const jonas = "I'm " + firstName + ", a " + (year - birthYear) + job ;
+// console.log(jonas);
+
+// const jonasNew = `Im ${firstName} a ${year - birthYear} year old ${job}`
+// console.log(jonasNew)
+
+// const age = 17;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//     console.log(`Sarah can start driving license`)
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young, wait ${yearsLeft} years`)
+// }
+
+// const inputYear = "1991";
+// console.log(Number(inputYear));
+// console.log(inputYear + 18);
+
