08bf230348afd9e53b68a13281043031451bcc52
angelarroyo <aac@wayakna.com>
2023-06-14 14:04:33 -0600

    Fundamentals done

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+'use stricit';
+
+// let hasDriversLicense = false;
+// const passTest = True;
+
+// if (passTest) hasDriverLicense = True;
+// if (hasDriversLicense) console.log("I can drive.")
+
+const jonas = {
+    firstName : "Jonas",
+    lastName : "Perez",
+    friends : ["Checo", "Luis", "Pedro"]
+}
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[1]}`)
